e7b34eec9ef3650e9dd8254c803b98b6ac4da480
Alice <alicelobinha2@gmail.com>
2021-09-02 13:41:20 -0300

    finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,43 @@
 import React from "react";
 
 const buttonA = <button>Histórico dos Clientes</button>
-const buttonB = <button>Botão 2ª</button>
+const buttonB = <button>Cadastrar Cliente</button>
 const hasCustomer = true;
 
 const App = () => {
+
+  const renderShowHistory = () => (
+    <div>
+      Clique no botão abaixo para vizualizar o histórico
+      <br />
+      {buttonA}
+    </div>                
+  )
+
+  const renderAddCustomer = () => (
+    <div>
+      Clique abaixo para cadastrar os clientes
+      <br />
+      {buttonB}
+    </div>
+  )
+
+    const showCustomer = () => {
+      if (!hasCustomer) return null
+      return (
+        <div><h2>Nome do Cliente: Alice Lobo</h2></div>
+      )
+    }
+
   return (
     <div>
       <p>Digital Innovation One</p>
       <p>Bem-vindo</p>
       <br />
-      {hasCustomer && (
-        <div>
-          Clique no botão abaixo para vizualizar o histórico
-          {buttonA}
-        </div>
-      )}
-      {buttonA}
-      {buttonB}
-
+      {hasCustomer ? renderShowHistory() : renderAddCustomer()}
+      <div>
+        {showCustomer()}
+      </div>
     </div>
   );
 };
